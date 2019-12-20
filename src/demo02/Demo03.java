8739b80d7af2b369b03afa255f79d4b47588680e
weizhao <1792926301@qq.com>
2019-12-20 13:49:41 +0800

    发布版本 0.1

---
+++ src/demo02/Demo03.java
@@ -0,0 +1,15 @@
+package demo02;
+
+import java.util.ArrayList;
+
+/**
+ * @author shkstart
+ * @data 2019/12/18-16:22
+ */
+public class Demo03 {
+    public static void main(String[] args) {
+        while (true){
+            new ArrayList<>();
+        }
+    }
+}
