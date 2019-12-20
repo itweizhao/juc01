024d7adfcd428b1f021b313cd3a9ccbd0d718115
weizhao <1792926301@qq.com>
2019-12-20 13:49:34 +0800

    发布版本 0.1

--- src/demo02/Demo02.java
+++ src/demo02/Demo02.java
@@ -1,5 +1,7 @@
 package demo02;
 
+import java.util.Random;
+
 /**
  * @author shkstart
  * @data 2019/12/17-20:41
@@ -11,6 +13,12 @@ public class Demo02 {
         long totalMemory = Runtime.getRuntime().totalMemory() ;//返回 Java 虚拟机中的内存总量。
         System.out.println("TOTAL_MEMORY(-Xms) = " + totalMemory + "（字节）、" + (totalMemory / (double)1024 / 1024) + "MB");
         System.out.println("MAX_MEMORY(-Xmx) = " + maxMemory + "（字节）、" + (maxMemory / (double)1024 / 1024) + "MB");
+        String str = "www.atguigu.com" ;
+        while(true)
+        {
+            str += str + new Random().nextInt(88888888) + new Random().nextInt(999999999) ;
+        }
+
     }
 
 
